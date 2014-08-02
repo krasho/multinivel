473ae584d5c1c784144a773a7fcb060126251902
Krasho <joseluis@balidevelop.com>
2014-08-02 16:44:24 -0500

    Mas cambios en el estilo del sitio

---
+++ app/assets/javascripts/site/site.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Krasho on 02/08/14.
+ */
+$(document).ready(function(){
+
+    //Acciones cuando presione uno de los elementos del menú
+    $("#menuLogin").click(function(e){
+        e.preventDefault();
+       $("#loggeo").toggle();
+    });
+
+
+});
--- config/application.rb
+++ config/application.rb
@@ -21,3 +21,5 @@ module Multinivel
     # config.i18n.default_locale = :de
   end
 end
+
+
