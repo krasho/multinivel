700bbca6202e5e45c9ce510df7cdb3f9d1219f4b
Krasho <joseluis@balidevelop.com>
2015-04-04 22:36:49 -0400

    using factories

---
+++ spec/factories/associate.rb
@@ -0,0 +1,12 @@
+#Mi firts factoty with Factory Girl
+FactoryGirl.define do 
+   factory :associate do 
+   	   name "Jose Luis"
+   	   phone "9838671532"
+   	   address "Conocida"
+
+   	   sequence(:email) {|n| "joseluis#{n}@yumalitas.com"}
+   	   supervisor_id "10"
+   	   city_id "23"
+   end
+end
--- spec/models/associate_spec.rb
+++ spec/models/associate_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe Associate do 
    context "Situaciones de invalidez" do
 		it "Es inválido sin el nombre" do 
-			socio = Associate.new(name: nil)
+			#socio = Associate.new(name: nil)
+			socio = FactoryGirl.build(:associate, name: nil)
 	        socio.valid?
 	        
 	        expect(socio.errors[:name]).to include("can't be blank")
@@ -11,29 +12,34 @@ describe Associate do
 
 
 		it "Es inválido sin el teléfono" do 
-			socio = Associate.new(phone: nil)
+			#socio = Associate.new(phone: nil)
+			socio = FactoryGirl.build(:associate, phone: nil)
 			socio.valid?
 
 			expect(socio.errors[:phone]).to include("can't be blank")
 		end 
 
 		it "Es inválido sin el correo" do 
-			socio = Associate.new(email: nil)
+			#socio = Associate.new(email: nil)
+			socio = FactoryGirl.build(:associate, email: nil)
 			socio.valid?
 
 			expect(socio.errors[:email]).to include("can't be blank")
 		end
 
 		it "Es inválido sin el supervisor" do 
-			socio = Associate.new(supervisor_id: nil)
+			#socio = Associate.new(supervisor_id: nil)
+			socio = FactoryGirl.build(:associate, supervisor_id: nil)
 			socio.valid?
 
 			expect(socio.errors[:supervisor_id]).to include("can't be blank")
 		end
 
 		it "Supervisor es inválido sino es número" do 
-			socio = Associate.new
-			socio.supervisor_id = "uno"
+			#socio = Associate.new
+			#socio.supervisor_id = "uno"
+
+            socio = FactoryGirl.build(:associate, supervisor_id: "uno")
 
 			socio.valid?
 			
@@ -42,15 +48,17 @@ describe Associate do
 
 
 		it "Es inválido sin la ciudad" do 
-			socio = Associate.new(city_id: nil)
+			#socio = Associate.new(city_id: nil)
+			socio = FactoryGirl.build(:associate, city_id: nil)
 			socio.valid?
 
 			expect(socio.errors[:city_id]).to include("can't be blank")
 		end
 
 		it "Ciudad es invália sino es un número" do 
-			socio = Associate.new
-			socio.city_id = "uno"
+			#socio = Associate.new
+			#socio.city_id = "uno"
+			socio = FactoryGirl.build(:associate, city_id: "uno")
             socio.valid?
 
             expect(socio.errors[:city_id]).to include("is not a number")
@@ -63,7 +71,8 @@ describe Associate do
                                 foo@bar_baz.com foo@bar+baz.com]
             
             invalid_addresses.each do |invalid_address|
-               socio = Associate.new
+               #socio = Associate.new
+               socio = FactoryGirl.build(:associate)
                socio.email = invalid_address
                socio.valid?
             
@@ -75,7 +84,8 @@ describe Associate do
 
    context "Situaciones válidas" do
    	   it "Supervisor válido si es número" do
-			socio = Associate.new
+			#socio = Associate.new
+			socio = FactoryGirl.build(:associate)
 			socio.supervisor_id = 1
 			socio.valid?
 
@@ -86,17 +96,22 @@ describe Associate do
             valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]            
             valid_addresses.each do |valid_address|
-               socio = Associate.new
-               socio.name  = "Jose"
-               socio.supervisor_id = 1
-               socio.city_id = 1
+               #socio = Associate.new
+               socio = FactoryGirl.build(:associate)
+               #socio.name  = "Jose"
+               #socio.supervisor_id = 1
+               #socio.city_id = 1
                socio.email = valid_address
-               socio.phone = "1111"
+               #socio.phone = "1111"
                socio.valid?
             
                expect(socio).to be_valid
              
             end			
+		end
+
+		it "Associate válido" do 
+			expect(FactoryGirl.build(:associate)).to be_valid
 		end
 
    end
