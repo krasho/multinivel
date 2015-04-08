5769558904e41fee394c595487c39758da6a7dcd
Krasho <joseluis@balidevelop.com>
2015-04-07 20:10:39 -0400

    Making a list of associates

--- spec/requests/associate_pages_spec.rb
+++ spec/requests/associate_pages_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 describe "AssociatePages", :type => :requests do
 	describe "Home Page" do 
 		context "List all associates" do 
-			let!(:associate1) {FactoryGirl.create(:associate)}
-			let!(:associate2) {FactoryGirl.create(:associate)}
-			let!(:associate3) {FactoryGirl.create(:associate)}
+			#let!(:associate1) {FactoryGirl.create(:associate)}
+			#let!(:associate2) {FactoryGirl.create(:associate)}
+			#let!(:associate3) {FactoryGirl.create(:associate)}
 
-			let (:socios){Associate.all}
+			#let (:socios){Associate.all}
+			
+			let!(:posts) { FactoryGirl.create_list(:associate, 3) }
 
             before do 
             	visit associates_path
