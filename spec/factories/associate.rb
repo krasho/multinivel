ddc1b8e45c635cff7cceb0dcdecf21ece577d1d0
Krasho <joseluis@balidevelop.com>
2015-04-07 20:05:12 -0400

    Using faker

--- spec/factories/associate.rb
+++ spec/factories/associate.rb
@@ -1,9 +1,9 @@
 #Mi firts factoty with Factory Girl
 FactoryGirl.define do 
    factory :associate do 
-   	   name "Jose Luis"
-   	   phone "9838671532"
-   	   address "Conocida"
+   	   name {Faker::Name.name}
+   	   phone {Faker::PhoneNumber.phone_number}
+   	   address {Faker::Address.street_address}
 
    	   sequence(:email) {|n| "joseluis#{n}@yumalitas.com"}
    	   supervisor_id 10
--- spec/requests/associate_pages_spec.rb
+++ spec/requests/associate_pages_spec.rb
@@ -7,23 +7,20 @@ describe "AssociatePages", :type => :requests do
 			let!(:associate2) {FactoryGirl.create(:associate)}
 			let!(:associate3) {FactoryGirl.create(:associate)}
 
+			let (:socios){Associate.all}
+
+            before do 
+            	visit associates_path
+            end 
+
 
 			it "should list all available associates names" do 
-				visit associates_path
-
-				socios = Associate.all
-
 				socios.each do |socio|
 					expect(page).to have_content socio.name
 				end 
 			end
 
 			it "should list all available associates email" do 
-
-				visit associates_path
-
-				socios = Associate.all
-
 				socios.each do |socio|
 					expect(page).to have_content socio.email
 				end 
