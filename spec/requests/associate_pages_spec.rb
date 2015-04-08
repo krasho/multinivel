9c2670a16b65d3b31168ad4a2b65291bb4327600
Krasho <joseluis@balidevelop.com>
2015-04-07 20:14:03 -0400

    Using Subject

--- spec/requests/associate_pages_spec.rb
+++ spec/requests/associate_pages_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe "AssociatePages", :type => :requests do
+
+    subject {
+    	page
+    }
+
 	describe "Home Page" do 
 		context "List all associates" do 
 			#let!(:associate1) {FactoryGirl.create(:associate)}
@@ -8,8 +13,8 @@ describe "AssociatePages", :type => :requests do
 			#let!(:associate3) {FactoryGirl.create(:associate)}
 
 			#let (:socios){Associate.all}
-			
-			let!(:posts) { FactoryGirl.create_list(:associate, 3) }
+
+			let!(:socios) { FactoryGirl.create_list(:associate, 3) }
 
             before do 
             	visit associates_path
@@ -18,13 +23,15 @@ describe "AssociatePages", :type => :requests do
 
 			it "should list all available associates names" do 
 				socios.each do |socio|
-					expect(page).to have_content socio.name
+					expect have_content socio.name
+					#expect(page).to have_content socio.name
 				end 
 			end
 
 			it "should list all available associates email" do 
 				socios.each do |socio|
-					expect(page).to have_content socio.email
+					expect have_content socio.email
+					#expect(page).to have_content socio.email
 				end 
 
 			end
