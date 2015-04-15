44c4a9a0dcb0a7b50ff43599477f757e3ad04ba4
Krasho <joseluis@balidevelop.com>
2015-04-15 09:11:32 -0400

    Bank's spec

---
+++ spec/requests/bank_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "BanksPages" , :type => :requests do
+	describe "Index page" do
+		describe "List All Banks" do 
+		end
+
+
+	end 
+
+
+	describe "Edit Page" do 
+	end
+
+
+	describe "Delete Page" do 
+	end
+
+
+	describe "Create Page" do 
+	end
+
+
+
+end 
