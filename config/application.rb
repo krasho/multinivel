856de6f7cd5e6b22e435bd69237021b7d2884db0
Krasho <joseluis@balidevelop.com>
2015-06-15 02:17:26 -0400

    Fix somethings of routes and confuguring

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Multinivel
+module MultinivelApi
   class Application < Rails::Application
 
     config.autoload_paths << Rails.root.join('lib')
--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,11 @@
-Rails.application.routes.draw do
+require 'api_constraints'
 
-  devise_for :users
-
-  post 'companies/supervisor', to: 'companies#supervisor'
-  get 'companies/supervisor', to: 'companies#supervisor'
-
-  resources :associates  
-  resources :banks
-  resources :cities
-  resources :commissions
-  resources :commission_types
-  resources :companies
-  resources :payment_types
-  resources :states
-
-  get 'principals/index', to: 'principals#index'
-
-  get 'site/:page' => 'site#pagina', as: :static_page
-
-  root 'site#index'
-
-  #Ruta para el registro de un nuevo promotor
-  get  'registro/new/:promotor' => 'site#registro'
-  get  'site/:socio/acceso'    => 'site#acceso'
-  post 'site/:socio/createUser'       => 'site#createUser'
-  get  'site/:socio/confirmacion' => 'site#confirmacion'
-  post 'registro/create'    => 'site#create'
-  
-
+MultinivelApi::Rails.application.routes.draw do
   # Api definition
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-    # We are going to list our resources here
-  end  
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1 do
+      # We are going to list our resources here
+    end
+  end
+
 end
