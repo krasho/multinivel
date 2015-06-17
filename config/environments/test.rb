418b895254cda388e0a5dcc5d6183fde68e938ec
Jose Luis <crasho1481@gmail.com>
2015-06-17 18:01:13 -0400

    Change in the spec files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-MultininivelApi::Application.configure do
+MultinivelApi::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
--- lib/spec/api_constraints_spec.rb
+++ lib/spec/api_constraints_spec.rb
@@ -1,20 +1,23 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe "ApiConstraints" , :type => :requests do
+describe ApiConstraints do
   let(:api_constraints_v1) { ApiConstraints.new(version: 1) }
   let(:api_constraints_v2) { ApiConstraints.new(version: 2, default: true) }
 
   describe "matches?" do
-
+    
     it "returns true when the version matches the 'Accept' header" do
-      request = double(host: 'api.multinivel.dev',
-                       headers: {"Accept" => "application/vnd.multinivel.v1"})
-      api_constraints_v1.matches?(request).should be_true
+      request = double(host: 'api.multinivel.dev', headers: {"Accept" => "application/vnd.multinivel.v1"})
+      
+      expect(api_constraints_v1.matches?(request)).to be_true 
+      #api_constraints_v1.matches?(request).should be_true
     end
 
     it "returns the default version when 'default' option is specified" do
       request = double(host: 'api.multinivel.dev')
-      api_constraints_v2.matches?(request).should be_true
+
+      expect(api_constraints_v2.matches?(request)).to be_true
+      #api_constraints_v2.matches?(request).should be_true
     end
   end
 end
