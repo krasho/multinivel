08c31099dd0294658667b63f2d304c7d4748f265
Jose Luis <crasho1481@gmail.com>
2015-06-26 14:15:08 -0400

    Fix erros in update method

--- app/controllers/api/v1/banks_controller.rb
+++ app/controllers/api/v1/banks_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BanksController < ApplicationController
 	before_action :set_bank, only: [:edit, :update, :destroy]
 	#before_action :authenticate_user!
+	protect_from_forgery with: :null_session
 
 	def index
 		@banks = Bank.all
@@ -30,43 +31,20 @@ class Api::V1::BanksController < ApplicationController
     end
  
     def create
-	    @bank = Bank.new(bank_params)
-
-	    respond_to do |format|
-	      if @bank.save
-	        format.html { redirect_to banks_path, notice: 'Registro Guardado Satisfactoriamente' }
-	        format.json { render :index, status: :created, location: @bank }
-	      else
-	        format.html { render :new }
-	        format.json { render json: @bank.errors, status: :unprocessable_entity }
-	      end
-	    end
     end
 
     def update 
-      @bank = Bank.find(params[:id])
+		@banks = Bank.all
 
-      respond_to do |format|
-      
-	      if @bank.update(bank_params)
-	        format.html {}
-	        format.json { render :index, status: :ok, location: @bank }
-	      else
-	        format.html {}
-	        format.json { render json: @bank.errors, status: :unprocessable_entity }
-	      end
-	  end    
+       respond_to do |f|
+          f.html {}
+          f.json { render json: @banks }
+       end        
 
     end
 
 
 	def destroy
-	   @bank = Bank.find(params[:id])
-	   @bank.destroy
-	   respond_to do |format|
-	      format.html { redirect_to banks_path, notice: 'El banco fue eliminado correctamente.' }
-	      format.json { head :no_content }
-	   end
 	end
 
 
