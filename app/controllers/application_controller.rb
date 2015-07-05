9e310172bc520b9d968b9e623b46a36b581d6481
Krasho <joseluis@balidevelop.com>
2015-07-04 21:16:45 -0400

    Create Authenticable method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
   include ActionController::MimeResponds
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   before_action :configure_permitted_parameters, if: :devise_controller? 
 
+  include Authenticable
   
   def after_sign_in_path_for(resource)
     '/principals/index'
