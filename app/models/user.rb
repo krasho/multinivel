c3fa8d307e0a344246e4bb311f04311acfd534e6
Krasho <joseluis@balidevelop.com>
2015-07-19 12:50:25 -0400

    I Add roles in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,15 @@ class User < ActiveRecord::Base
   validates :auth_token, uniqueness: true          
 
   before_create :generate_authentication_token!
+  enum role: [:admin, :client, :supervisor]
 
   def generate_authentication_token!
     begin
       self.auth_token = Devise.friendly_token
     end while self.class.exists?(auth_token: auth_token)
   end
+
+  def set_default_role
+      self.role ||= :client
+    end
 end
