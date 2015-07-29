db1e04785d44e0bb00de6bfd8a1d8dcac120f443
Jose Luis <crasho1481@gmail.com>
2015-07-29 13:52:55 -0400

    Fix errors in the rspec

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  #describe "Factory" do
-  #	let(:city) {FactoryGirl.build(:city)}
+  describe "Factory" do
+  	let(:city) {FactoryGirl.build(:city)}
 
-  #	it "factory should be valid" do
-  #    expect(city).to be_valid
-  #  end
-  #end
+  	it "factory should be valid" do
+      expect(city).to be_valid
+    end
+  end
 end
