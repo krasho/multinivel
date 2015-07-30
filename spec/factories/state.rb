81a4ebbd3defafc5b759e3101cace1df5b05d459
Jose Luis <crasho1481@gmail.com>
2015-07-30 17:43:04 -0400

    I change city factory

--- spec/factories/state.rb
+++ spec/factories/state.rb
@@ -1,5 +1,17 @@
 FactoryGirl.define do 
 	factory :state do 
 		name {Faker::Address.city}
+
+
+	trait :with_city_list do
+      transient do
+        number_of_cities 1
+      end
+
+      after(:create) do |state, evaluator|
+        FactoryGirl.create_list(:cities, evaluator.number_of_cities, state: state)
+      end
+    end
+
 	end
 end
